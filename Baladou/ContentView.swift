70e037867e6d4e02bb8343caa2b9f0d897c0a8da
steph29 <stephane.verardo@hotmail.fr>
2020-01-03 15:45:35 +0100

    Initial Commit

---
+++ Baladou/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Baladou
+//
+//  Created by stephane verardo on 03/01/2020.
+//  Copyright © 2020 stephane verardo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
